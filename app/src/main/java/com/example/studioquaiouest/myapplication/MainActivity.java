0d882753cbbaa311934684d18effc00c624a5a77
thomas40510 <thomasprevost85@gmail.com>
2017-07-27 17:52:36 +0200

    achristophe-story 27/07@17:53

--- app/src/main/java/com/example/studioquaiouest/myapplication/MainActivity.java
+++ app/src/main/java/com/example/studioquaiouest/myapplication/MainActivity.java
@@ -232,7 +232,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void gotoLB (View view){
-        Intent intent = new Intent(this, screen45.class);
+        Intent intent = new Intent(this, screen49.class);
         startActivity(intent);
     }
 
--- app/src/main/java/com/example/studioquaiouest/myapplication/screen49.java
+++ app/src/main/java/com/example/studioquaiouest/myapplication/screen49.java
@@ -1,9 +1,13 @@
 package com.example.studioquaiouest.myapplication;
 
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class screen49 extends AppCompatActivity {
 
@@ -12,6 +16,33 @@ public class screen49 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen49);
     }
+
+    View view;
+    int radioId;
+
+    public void Verif (View view){
+
+        RadioGroup group = (RadioGroup) findViewById(R.id.radioGroup2);
+        radioId = group.getCheckedRadioButtonId();
+
+        if (radioId == R.id.radio4){
+            findViewById(R.id.txtWrong).setVisibility(View.INVISIBLE);
+            RadioButton radio = (RadioButton) findViewById(radioId);
+            radio.setChecked(false);
+            gotoNext(view);
+        } else {
+            Wrong();
+        }
+    }
+
+    public void Wrong(){
+        findViewById(R.id.txtWrong).setVisibility(View.VISIBLE);
+        RadioButton radio = (RadioButton) findViewById(radioId);
+        radio.setChecked(false);
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        v.vibrate(300);
+    }
+
     public void gotoNext (View view){
         Intent intent = new Intent(this, resume.class);
         int i = 51;
