3dfbf69ab90f88ae700dac661cca1a24be9b8195
thomas40510 <thomasprevost85@gmail.com>
2017-07-27 18:46:53 +0200

    achristophe-story 27/07@18:47

--- app/src/main/java/com/example/studioquaiouest/myapplication/MainActivity.java
+++ app/src/main/java/com/example/studioquaiouest/myapplication/MainActivity.java
@@ -232,7 +232,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void gotoLB (View view){
-        Intent intent = new Intent(this, screen49.class);
+        Intent intent = new Intent(this, screen25.class);
         startActivity(intent);
     }
 
--- app/src/main/java/com/example/studioquaiouest/myapplication/screen55.java
+++ app/src/main/java/com/example/studioquaiouest/myapplication/screen55.java
@@ -1,6 +1,8 @@
 package com.example.studioquaiouest.myapplication;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +14,18 @@ public class screen55 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen55);
     }
+
+    public void isWrong(View view){
+        findViewById(R.id.imgWrong).setVisibility(View.VISIBLE);
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        v.vibrate(300);
+    }
+
+    public void isTrue (View view){
+        findViewById(R.id.imgWrong).setVisibility(View.INVISIBLE);
+        gotoNext(view);
+    }
+
     public void gotoNext (View view){
         Intent intent = new Intent(this, screenshots.class);
         int i = 57;
